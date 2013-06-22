b00345a9c34ae0b2b20e135a33c966e65ef2cbe7
mo khan <mo@mokhan.ca>
2013-06-22 11:08:00 -0600

    add gravatar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    image_tag("https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}&d=mm", alt: user.email, class: 'gravatar')
+  end
 end
