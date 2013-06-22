3cbbe067d9c508c25ecb8608a65ed2fcf7624a4b
mo khan <mo@mokhan.ca>
2013-06-22 09:34:24 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yycrebuild::Application.config.secret_token = '6ec4838e09a0d3c8f1815996d903c94dbc893b7d52baee1f89cf3edf456f395fc68301e98cb90232cb7ca91949f5dea9657362b7021b932699f98e6b02dce6c0'
