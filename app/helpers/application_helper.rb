a351b6133a7b6a6e1206a511c80e269d9b63b52a
mo khan <mo@mokhan.ca>
2013-06-22 19:30:51 -0600

    list out name and move tags to separate column

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def gravatar_for(user, options = { size: 80 })
+  def gravatar_for(user, options = { size: 40 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     image_tag("https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}&d=mm", alt: user.name, class: 'gravatar')
   end
