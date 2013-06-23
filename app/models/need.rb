727b548d956038c4bd5c2843293dce6c975c9fee
mo khan <mo@mokhan.ca>
2013-06-22 23:05:20 -0600

    geocode entered address

--- app/models/need.rb
+++ app/models/need.rb
@@ -2,4 +2,6 @@ class Need < ActiveRecord::Base
   belongs_to :user
   attr_accessible :description, :address, :latitude, :longitude
   acts_as_taggable
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
 end
