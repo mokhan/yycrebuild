347ca68a37dc8eb548d06e041e5ec2240d2de891
mo khan <mo@mokhan.ca>
2013-06-23 00:38:08 -0600

    extract helper method to determine which css class to apply

--- app/helpers/neighbourhoods_helper.rb
+++ app/helpers/neighbourhoods_helper.rb
@@ -1,2 +1,12 @@
 module NeighbourhoodsHelper
+  def css_class_for(neighbourhood)
+    case
+    when neighbourhood.status.start_with?('Evac')
+      "evac"
+    when neighbourhood.status.start_with?('Part')
+      "part"
+    else
+      "open"
+    end
+  end
 end
