de3c7de2bac4f9f27ee7b74567c234177cd41d48
mendicantx <mendicant@beigesunshine.com>
2013-06-23 22:53:12 -0600

    Add location to a user via a postal code.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,24 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  after_save :get_location
 
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :postal_code, :latitude, :longitude
   has_many :needs
   acts_as_tagger
+
+private
+
+  def get_location
+
+    return if self.postal_code.nil? 
+  
+    location = GeoLocationService.GetGeoLocation(self.postal_code)
+
+    return if location.nil?
+
+    update_column :latitude, location.y
+    update_column :longitude, location.x
+    
+  end 
+  
 end
